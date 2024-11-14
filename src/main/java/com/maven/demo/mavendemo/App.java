0129981c449ae86e0c2006d0fdf85bcc1f2b7989
Shresta-Voruganti <22bd1a053z@gmail.com>
2024-11-14 12:19:34 +0530

    added files

---
+++ src/main/java/com/maven/demo/mavendemo/App.java
@@ -0,0 +1,13 @@
+package com.maven.demo.mavendemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Shresta Voruganti - 22BD1A053Z" );
+    }
+}
